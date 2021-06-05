36474cc129f9a019ab676ed4c1f007983d175e1f
# Chris Lee <minjaelee0727@gmail.com>
2021-06-05 17:06:27 +0900

    Refactor SPPModel.py

--- ai/SPPModel.py
+++ ai/SPPModel.py
@@ -24,6 +24,7 @@ plt.rcParams['font.family'] = 'Malgun Gothic'
 class DataNotEnough(BaseException):
     pass
 
+
 # 학습 함수
 def train(data, model, n_epochs=400, batch_size=64, verbose=0):
     early_stopping = EarlyStopping(monitor='val_loss', patience=50)  # 50번이상 더 좋은 결과가 없으면 학습을 멈춤
@@ -39,11 +40,13 @@ def train(data, model, n_epochs=400, batch_size=64, verbose=0):
 
     return history
 
+
 # 에러 평가 함수
 def evaluate(data, model):
     mse, mae = model.evaluate(data["X_test"], data["y_test"], verbose=0)
     mean_absolute_error = data["column_scaler"]["close"].inverse_transform([[mae]])[0][0]
     return mean_absolute_error
+
 
 # 예측 주가를 계산 해주는 함수
 def predict(data, model, n_steps=100):
@@ -59,6 +62,7 @@ def predict(data, model, n_steps=100):
     predicted_price = column_scaler["close"].inverse_transform(prediction)[0][0]
 
     return predicted_price
+
 
 # 스케일링 and "X_train", "X_test", "y_train", "y_test" 추출 함수
 def load_data(df, n_steps=100, lookup_step=1, test_size=0.2, shuffle=True):
@@ -116,6 +120,7 @@ def load_data(df, n_steps=100, lookup_step=1, test_size=0.2, shuffle=True):
 
     return result
 
+
 # 모델 생성 함수
 def create_model(units=50, dropout=0.3, n_steps=100, loss='mae', optimizer='adam', n_layers=4, cell=LSTM):
     model = Sequential()
@@ -132,6 +137,7 @@ def create_model(units=50, dropout=0.3, n_steps=100, loss='mae', optimizer='adam
     model.compile(loss=loss, metrics=[loss], optimizer=optimizer)
 
     return model
+
 
 # 그래프 출력 함수
 def plot_graph(model, data):
